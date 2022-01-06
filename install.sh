dca2b68400c22ddb89a771ff466f0c6484b3881d
hacker41d4n <54631543+hacker41d4n@users.noreply.github.com>
2022-01-06 15:12:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,9 @@ echo "------------------------------"
 echo "--           Music          --"
 echo "------------------------------"
 
-sudo apt install spotify-client
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
 
 echo "------------------------------"
 echo "--           Video          --"
@@ -40,7 +42,6 @@ echo "--           Office          --"
 echo "-------------------------------"
 
 sudo apt install libreoffice
-sudo apt install teams
 
 echo "------------------------------"
 echo "--           Email          --"
