9baeaef3bae8de101e7f15154b02ffcaa5434d18
hacker41d4n <54631543+hacker41d4n@users.noreply.github.com>
2022-01-07 11:11:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,9 +17,9 @@ echo "------------------------------"
 echo "--           Music          --"
 echo "------------------------------"
 
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
+sudo apt-get update && sudo apt install spotify-client -y
 
 echo "------------------------------"
 echo "--           Video          --"
